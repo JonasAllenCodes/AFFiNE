6be02ff19d8aa6fa6217b1ecc7e787b3db92cb1c
Jimmfly <yangjinfei001@gmail.com>
2024-08-12 22:16:55 +0800

    feat(component): add tabs ui component

---
+++ packages/frontend/component/src/ui/tabs/tabs.css.ts
@@ -0,0 +1,52 @@
+// src/components/index.css.ts
+import { cssVar } from '@toeverything/theme';
+import { style } from '@vanilla-extract/css';
+
+export const tabsRoot = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  gap: '8px',
+});
+
+export const tabsList = style({
+  display: 'flex',
+  gap: '12px',
+  boxSizing: 'border-box',
+  position: 'relative',
+  '::after': {
+    content: '""',
+    position: 'absolute',
+    bottom: '0px',
+    width: '100%',
+    height: '1px',
+    backgroundColor: cssVar('borderColor'),
+  },
+});
+
+export const tabsTrigger = style({
+  all: 'unset',
+
+  padding: '6px 4px',
+  cursor: 'pointer',
+  fontSize: cssVar('fontSm'),
+  color: cssVar('textSecondaryColor'),
+  borderBottom: '2px solid transparent',
+  selectors: {
+    '&[data-state="active"]': {
+      color: cssVar('textPrimaryColor'),
+      borderColor: cssVar('primaryColor'),
+    },
+  },
+});
+
+export const tabsContent = style({
+  display: 'flex',
+  flexDirection: 'column',
+
+  selectors: {
+    '&[data-state="inactive"]': {
+      display: 'none',
+    },
+  },
+});
